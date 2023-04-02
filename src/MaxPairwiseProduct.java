371aada8dc41f4e1d650c332224a409763c4046b
Amos Thando Mpofu <56162910+theliteprogrammer@users.noreply.github.com>
2023-04-02 20:41:10 +0200

    Update MaxPairwiseProduct.java

--- src/MaxPairwiseProduct.java
+++ src/MaxPairwiseProduct.java
@@ -1,5 +1,71 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.*;
+import java.io.*;
+
+public class MaxPairwiseProduct {
+    static long getMaxPairwiseProduct(int[] numbers) {
+        long max_product = 0;
+        int max_number1 = 0;
+        int max_number2 = 0;
+        int max_1 = 0;
+        int max_2 = 0;
+        int n = numbers.length;
+
+        // find index of the first maximum
+        for (int i = 0; i < n; i++) {
+            if (numbers[i] > max_number1) {
+                max_number1 = numbers[i];
+                max_1 = i;
+            }
+        }
+
+        // find the index of the second maximum
+        for (int i = 0; i < n; i++) {
+            if ((i != max_1) && (numbers[i] > max_number2)) {
+                max_number2 = numbers[i];
+                max_2 = i;
+            }
+        }
+        max_product = Math.max(max_product, ((long)numbers[max_1]) * numbers[max_2]);
+        return max_product;
     }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(getMaxPairwiseProduct(numbers));
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new
+                        InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
 }
